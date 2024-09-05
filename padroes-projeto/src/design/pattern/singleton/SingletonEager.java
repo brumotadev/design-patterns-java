813c802478072fb24e207c5c7c779d9ff663a98c
Bruno Mota <bmotadev@gmail.com>
2024-09-05 17:19:28 -0300

    Singleton Eager

--- padroes-projeto/src/design/pattern/singleton/SingletonEager.java
+++ padroes-projeto/src/design/pattern/singleton/SingletonEager.java
@@ -2,4 +2,13 @@ package design.pattern.singleton;
 
 public class SingletonEager {
 
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
 }
