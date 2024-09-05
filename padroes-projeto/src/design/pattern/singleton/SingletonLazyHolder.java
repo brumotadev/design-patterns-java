e13c2d5b0f1ac07b968d0886d8b24718186f6998
Bruno Mota <bmotadev@gmail.com>
2024-09-05 17:27:18 -0300

    Design Pattern Singleton com Java

---
+++ padroes-projeto/src/design/pattern/singleton/SingletonLazyHolder.java
@@ -0,0 +1,25 @@
+package design.pattern.singleton;
+
+/**
+ * Singleton "Lazy Holder"
+ * 
+ * 
+ * 
+ */
+
+
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
